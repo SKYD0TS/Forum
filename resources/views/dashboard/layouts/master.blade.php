4fc9db0dfc0dc0269d1a8e1a0d38919036c0e4ab
SKYD0TS <SKYD0TS@users.noreply.github.com>
2023-01-04 21:56:51 +0700

    discard changes

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -9,7 +9,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Custom styles for this template -->
+
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}" />
+
+
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
 
     <style>
@@ -35,8 +38,6 @@
             </main>
         </div>
     </div>
-
-
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
