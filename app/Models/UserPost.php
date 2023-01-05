455ec80715be37c5cfdbe5bc4422685ac1d5b38e
SKYD0TS <SKYD0TS@users.noreply.github.com>
2023-01-05 23:50:58 +0700

    UserPost Model

---
+++ app/Models/UserPost.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPost extends Model
+{
+    use HasFactory;
+    protected $table = 'users_posts';
+    protected $guarded = ['id'];
+    public $timestamps = false;
+}
